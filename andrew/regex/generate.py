9af5f2fb05e4a0fe5288b1d57cf79d8fc9394704
AndrewMorris-scsu <moan1303@stcloudstate.edu>
2016-09-30 15:00:44 -0500

    test files for csci club meeting

---
+++ andrew/regex/generate.py
@@ -0,0 +1,25 @@
+import random
+import string
+domains = ['@stcloudstate.edu', '@gmail.com', '@hotmail.com', '@aol.com', '@netzero.com', '@fbi.gov']
+file = open('testfile.html', 'w')
+
+numRecords = 100
+
+for i in range(numRecords):
+    rand = random.random()
+    email = ""
+    if rand < 0.5:
+        #OldSchool
+        for j in range(0,4):
+            email += string.ascii_lowercase[random.randrange(0,26)]
+        for k in range(0,4):
+            email += str(random.randrange(0,10))
+    else:
+        #newSchool
+        for j in range(random.randrange(8,12)):
+            email += string.ascii_lowercase[random.randrange(0,26)]
+    
+    
+    email += domains[random.randrange(0,len(domains))]
+
+    file.write((email + ' '))
---
+++ andrew/regex/test.py
@@ -0,0 +1,3 @@
+import re
+
+print (re.split(r'b*','abc \n abc'))
